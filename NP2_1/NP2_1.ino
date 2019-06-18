3800d396fa68a74710388924bcba0c606893c60c
Jovani <jovanidesouza@hotmail.com>
2019-06-18 18:33:23 -0300

    Trabalhos np2

---
+++ NP2_1/NP2_1.ino
@@ -0,0 +1,132 @@
+/*
+ * Trabalho NP2.1 - Optativa: Tópicos em computação IV
+ * Implementar solução para o problema Sleeping Barber Problem
+ * Utilizando mutex e semáfaros
+ * SO FreeRTOS rodando no Arduíno UNO.
+ * 
+ * Jovani de Souza - 1311100017
+ * Ciência da Computação - UFFS
+ * 
+ */
+
+ //#define INCLUDE_vTaskSuspend     1 / in FreeRTOSConfig.h
+
+#include "FreeRTOS_AVR.h"
+#include "basic_io_avr.h"
+
+
+static void main_task(void *pvParameters );//task cliente
+
+static void barber_task(void *pvParameters);//task barbeiro
+
+
+SemaphoreHandle_t xMutex;//mutex
+SemaphoreHandle_t xSemaphore;//counting semafaro
+
+TaskHandle_t xHandleBarbeiro;// handle da task barbeiro
+
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  Serial.begin(9600);
+
+  xMutex = xSemaphoreCreateMutex(); 
+  xSemaphore = xSemaphoreCreateCounting( 2, 2 );
+  
+
+  if(xMutex != NULL && xSemaphore != NULL ){
+
+    xTaskCreate( main_task, "CLIENTE1", 100, "cliente 1", 2, NULL );
+     
+    xTaskCreate( main_task, "CLIENTE2", 100, "cliente 2", 2, NULL );
+
+    xTaskCreate( main_task, "CLIENTE3", 100, "cliente 3", 2, NULL );
+
+    xTaskCreate( main_task, "CLIENTE4", 100, "cliente 4", 2, NULL );
+
+    xTaskCreate( main_task, "CLIENTE5", 100, "cliente 5", 2, NULL );
+     
+    //xTaskCreate( main_task, "CLIENTE6", 100, "cliente 6", 2, NULL );
+
+    //xTaskCreate( main_task, "CLIENTE7", 100, "cliente 7", 2, NULL );
+
+    //xTaskCreate( main_task, "CLIENTE8", 100, "cliente 8", 2, NULL );
+    
+
+
+    xTaskCreate(barber_task, "BARBEIRO", 100, "barbeiro", 1, &xHandleBarbeiro);
+
+  
+    vTaskStartScheduler();
+  
+  }else{
+     vPrintString("Erro ao criar Mutex e semáfaro\n");
+  }
+
+  for(;;){}
+
+}
+
+static void main_task( void *pvParameters ){//terafa dos clientes
+
+  const char *pcTaskName = (char *) pvParameters ;
+  const TickType_t xDelay = 1000 / portTICK_PERIOD_MS;
+  int rand_esperar = 1000;
+  int rand_voltar = 10000;
+
+  rand_voltar = random(5000 , 10000);
+  rand_esperar = random(1000 , 4000);
+  vTaskDelay(rand_esperar / portTICK_PERIOD_MS);//coloca as tasks imediatamente para esperar um tempo randomico antes de irem para o barbeiro
+
+  for(;;){
+
+    //code cliente here
+    
+    if(xSemaphoreTake( xSemaphore,  pdMS_TO_TICKS(100) ) == pdTRUE){//se conseguir um lugar na fila
+      //vPrintString("\n acorde.\n");
+      vTaskResume( xHandleBarbeiro);//acorda o barbeiro
+       
+       if(xSemaphoreTake( xMutex,  portMAX_DELAY ) == pdTRUE){//tenta cortar o cabelo pegando o mutex
+        
+        xSemaphoreGive( xSemaphore );//libera o lugar dele na fila
+        vPrintString((char *) pvParameters );
+        vPrintString("\nCliente está sendo atendido.\n");
+        vTaskDelay(xDelay*2);//tempo para cortar o cabelo
+        xSemaphoreGive( xMutex );//devolve o mutex para o proximo ser atendido
+        //vTaskDelay(rand_voltar / portTICK_PERIOD_MS);//tempo que leva para voltar à barbearia
+        vTaskDelay(xDelay*10);//alterar para rand para variar o tempo de volta a barbearia
+       }
+    }else{
+      
+      vTaskDelay(rand_voltar / portTICK_PERIOD_MS);//vai entra nesse else, caso não tenha lugar na barbearia, espera um tempo e retorna para a barbearia
+    
+    }
+      
+  }//fim do loop
+  
+}//fim da tarefa clientes
+
+static void barber_task(void *pvParameters){
+
+  const char *pcTaskName = (char *) pvParameters ;
+
+   for(;;){
+    
+      vTaskDelay(50 / portTICK_PERIOD_MS);
+      if(xSemaphoreTake( xMutex,  portMAX_DELAY ) == pdTRUE){//condição para o barbeiro dormir
+        
+        vPrintString("\nBarbeiro: vou dormir.\n\n");
+        xSemaphoreGive( xMutex );
+        vTaskSuspend(xHandleBarbeiro);
+      }
+   
+   }
+}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ NP2_2/NP2_2.ino
@@ -0,0 +1,162 @@
+/*
+ * Trabalho NP2.2 - Optativa: Tópicos em computação IV
+ * Implementar solução para o problema Sleeping Barber Problem
+ * Utilizando task notification
+ * SO FreeRTOS rodando no Arduíno UNO.
+ * 
+ * Jovani de Souza - 1311100017
+ * Ciência da Computação - UFFS
+ * 
+ */
+
+
+#include "FreeRTOS_AVR.h"
+#include "basic_io_avr.h"
+
+
+static void cliente_task(void *pvParameters );//task cliente
+static void barber_task(void *pvParameters);//task barbeiro
+
+TaskHandle_t xHandleBarbeiro;// handle da task barbeiro
+TaskHandle_t xCliente1;
+TaskHandle_t xCliente2;
+TaskHandle_t xCliente3;
+TaskHandle_t xCliente4;
+//TaskHandle_t xCliente5;
+
+int vagas = 2;
+
+QueueHandle_t fila;
+
+const TickType_t xDelay = 1000 / portTICK_PERIOD_MS;
+
+SemaphoreHandle_t xMutex;//mutex
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+
+  
+  fila = xQueueCreate( 2, sizeof(TaskHandle_t * ) );
+  xMutex = xSemaphoreCreateMutex(); 
+  
+
+  if(fila != NULL){
+    
+    xTaskCreate( cliente_task, "CLIENTE1", 100, "cliente 1", 2, &xCliente1 );
+     
+    xTaskCreate( cliente_task, "CLIENTE2", 100, "cliente 2", 2, &xCliente2 );
+
+    xTaskCreate( cliente_task, "CLIENTE3", 100, "cliente 3", 2, &xCliente3 );
+
+    xTaskCreate( cliente_task, "CLIENTE4", 100, "cliente 4", 2, &xCliente4 );
+
+    //xTaskCreate( cliente_task, "CLIENTE5", 100, "cliente 5", 2, &xCliente5 );
+
+
+    xTaskCreate(barber_task, "BARBEIRO", 100, "barbeiro", 1, &xHandleBarbeiro);
+
+
+    vTaskStartScheduler();
+  
+  
+  }else{
+     vPrintString("Erro ao criar fila\n");
+  }
+
+  for(;;){}
+
+  
+
+}//fim setup
+
+static void cliente_task(void *pvParameters ){
+
+  const char *pcTaskName = (char *) pvParameters ;
+  
+  int rand_esperar = 1000;
+  
+  int x, y;
+  rand_esperar = random(1000 , 4000);
+  
+  vTaskDelay(rand_esperar / portTICK_PERIOD_MS);//coloca as tasks imediatamente para esperar um tempo randomico antes de irem para o barbeiro
+
+  for(;;){
+    
+    if(vagas > 0){
+      vagas--;
+      
+      if(pcTaskName == "cliente 1"){
+        xQueueSend( fila, ( void * ) &xCliente1, 0 );
+      }else if(pcTaskName == "cliente 2"){
+        xQueueSend( fila, ( void * ) &xCliente2, 0 );
+      }else if(pcTaskName == "cliente 3"){
+        xQueueSend( fila, ( void * ) &xCliente3, 0 );
+      }else if(pcTaskName == "cliente 4"){
+        xQueueSend( fila, ( void * ) &xCliente4, 0 );
+      }
+    
+
+      xTaskNotifyGive(xHandleBarbeiro);
+
+      x = ulTaskNotifyTake( pdTRUE, portMAX_DELAY );//bloqueio
+      vagas++;
+      vPrintString("\n\nbarbeiro atendendo o cliente:\n\n");
+      vPrintString((char *) pvParameters );
+
+      y = ulTaskNotifyTake( pdTRUE, portMAX_DELAY );//bloqueio
+      vPrintString("\n\nserviço finalizado.\n\n");
+      vTaskDelay(xDelay*10);//tempo para precisar do serviço novamente
+
+    }else{//volta mais tarde
+      vPrintString("\n\n");
+      vPrintString((char *) pvParameters );
+      vPrintString("\nvolto depois\n");
+      vTaskDelay(xDelay*5);
+    }
+    
+    
+    //if(xSemaphoreTake( xMutex,  portMAX_DELAY ) == pdTRUE){
+      //vPrintString((char *) pvParameters );
+      //vTaskDelay(xDelay);
+      //xTaskNotifyGive(xHandleBarbeiro);
+      //vTaskDelay(xDelay);
+      //xSemaphoreGive( xMutex );
+      //vTaskDelay(xDelay*5);
+    //}
+
+  }
+
+}//fim task cliente
+
+static void barber_task(void *pvParameters ){
+
+  TaskHandle_t corte;
+  int Wakeup = 0;
+  
+  for(;;){
+    
+    Wakeup = ulTaskNotifyTake( pdTRUE, portMAX_DELAY );//max wakep == tamanhop da fila
+
+    if( Wakeup != 0 ){
+      Wakeup--;
+      xQueueReceive( fila, &( corte ), portMAX_DELAY);//pega da fila o primeiro que vai ser atendido
+      xTaskNotifyGive( corte );//notifica o cliente que ele sera atendido
+      vTaskDelay(xDelay*2);//tempo de corte
+      xTaskNotifyGive( corte );//notifica o cliente que o serviço está finalizado
+      
+      
+    }else{
+       vPrintString("\nBarbeiro: vou dormir.\n\n"); 
+    }
+    
+  }
+
+}//fim task barbeiro
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
